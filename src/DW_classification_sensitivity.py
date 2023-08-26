ab07a887303debd4e87c2e58014d295b6f902e53
James Bannon <j.bannon90@gmail.com>
2023-08-25 23:09:54 -0400

    changed to rebalance the dataset

--- src/DW_classification_sensitivity.py
+++ src/DW_classification_sensitivity.py
@@ -19,7 +19,7 @@ from sklearn.preprocessing import  StandardScaler
 
 
 from sklearn.pipeline import Pipeline
-from sklearn.model_selection import train_test_split,GridSearchCV
+from sklearn.model_selection import train_test_split,GridSearchCV,StratifiedKFold
 
 from sklearn.metrics import mean_absolute_error, mean_squared_error, roc_auc_score, confusion_matrix, accuracy_score,f1_score
 from sklearn.metrics import balanced_accuracy_score, precision_recall_curve, auc 
@@ -34,15 +34,18 @@ from tdc.single_pred import ADME
 
 from sklearn.neighbors import KNeighborsClassifier
 
+from imblearn.pipeline import Pipeline
+from imblearn.over_sampling import SMOTE
+from imblearn.under_sampling import RandomUnderSampler
 
 
 def main():
 	dataset = 'PAMPA_NCATS'
 
-	num_trials = 50
+	num_trials = 5
 
-	MAX_VERTEX_SCALES = 5
-	MAX_VERTEX_MOMENTS = 5
+	MAX_VERTEX_SCALES = 3
+	MAX_VERTEX_MOMENTS = 3
 
 	tdc_path = "../data/raw/tdc"
 	short_size = 32
@@ -57,7 +60,7 @@ def main():
 
 
 	svc_grid = {prefix + "__C":[10**i for i in range(-6,1)]}
-	lr_grid = {prefix + "__C":[10**i for i in range(-6,-3)]}
+	lr_grid = {prefix + "__C":[10**i for i in range(-6,-4)]}
 	
 	# sys.exit(1)
 
@@ -77,10 +80,13 @@ def main():
 
 
 	data = ADME(name = dataset, path = tdc_path) # will download if not already present
-	dataframe = data.get_data()	
-	data_set = dataset_utils.make_dataset(dataframe,short_size,long_size)
+	
+	
+	
+	
 
-	obs_index = list(data_set.keys())
+
+	
 	path = "../results/sensitivity/classification/"
 	os.makedirs(path,exist_ok=True)
 
@@ -92,92 +98,70 @@ def main():
 	rng = np.random.RandomState(1234)
 	results = defaultdict(list)	
 	feature_type = 'DW'
-	for max_vertex_scale in range(4,MAX_VERTEX_SCALES):
-			for max_vertex_moment in range(4,MAX_VERTEX_MOMENTS):
-				for center_vertex_features in [True, False]:
-					io_utils.star_echo("\nWorking on:\n\tvertex scale {J}\n\tvertex moment {p}\n\tcentering: {c}\n".format(J=max_vertex_scale,p = max_vertex_moment, c= center_vertex_features))
-					for i in tqdm.tqdm(range(num_trials)):
+	splitter = StratifiedKFold(n_splits = num_trials)
 
-						trn_idx, test_idx = train_test_split(obs_index ,random_state = rng)
-						train_ds = {i:data_set[i] for i in trn_idx}
-						test_ds = {i:data_set[i] for i in test_idx}
 
-						X_train, y_train = dataset_utils.make_numpy_dataset(train_ds,feature_type, 
-							max_vertex_scale, max_vertex_moment, center_vertex_features,
-							numScales_e = None, maxMoment_e = None, central_e = None
-							)
-						X_test, y_test  = dataset_utils.make_numpy_dataset(test_ds,feature_type,
-							max_vertex_scale, max_vertex_moment, center_vertex_features,
-							numScales_e = None, maxMoment_e = None, central_e = None
-							)
-						
-						for model in tqdm.tqdm(model_names,leave=False,desc = 'simple models'):
-							if model in ['SVC','RBF']:
-								grid = svc_grid
-							else:
-								grid = lr_grid
-							
-							cv_model = GridSearchCV(simple_tuples[model], grid)
-							cv_model.fit(X_train, y_train)
+	for dtype in ['full','approved']:
+		if dtype == 'full':
+			dataframe = data.get_data()
+		elif dtype == 'approved':
+			dataframe = data.get_approved_set()
+		data_set = dataset_utils.make_dataset(dataframe,short_size,long_size)
+		obs_index = list(data_set.keys())
+
+		results = defaultdict(list)
+		for max_vertex_scale in range(2,MAX_VERTEX_SCALES):
+				for max_vertex_moment in range(2,MAX_VERTEX_MOMENTS):
+					for center_vertex_features in [True, False]:
+						X,y = dataset_utils.make_numpy_dataset( {i:data_set[i] for i in obs_index},feature_type, 
+								max_vertex_scale, max_vertex_moment, center_vertex_features,
+								numScales_e = None, maxMoment_e = None, central_e = None
+								)
+						io_utils.star_echo("\nWorking on:\n\tvertex scale {J}\n\tvertex moment {p}\n\tcentering: {c}\n".format(J=max_vertex_scale,p = max_vertex_moment, c= center_vertex_features))
 						
 
-						
-
-							results['iter'].append(i)
-							results['model'].append(model)
-							results['max_vertex_scale'].append(max_vertex_scale)
-							results['max_vertex_moment'].append(max_vertex_moment)
-							results['centered_vertex_features'].append(center_vertex_features)
-							results['pca'].append("No")
-
+						for i, (train_idx, test_idx) in tqdm.tqdm(enumerate(splitter.split(X,y))):
+							steps = [('o', SMOTE(sampling_strategy = 0.9)),('u', RandomUnderSampler())]
+							samplePipe = Pipeline(steps)
+							X_train, y_train = X[train_idx,:], y[train_idx]
+							X_test, y_test = X[test_idx,:], y[test_idx]
+							
+							if dtype == 'full':
+								X_train, y_train = samplePipe.fit_resample(X_train,y_train)
 							
 							
-							preds = cv_model.best_estimator_.predict(X_test)
-							acc = accuracy_score(y_test, preds)
-							b_acc = balanced_accuracy_score(y_test,preds)
-
-							results['acc'].append(acc)
-							results['bal_acc'].append(b_acc)
-
-							if model == 'LR':
-								prob_preds = cv_model.best_estimator_.predict_proba(X_test)
-								roc_auc = roc_auc_score(y_test, prob_preds[:,1])
-								prec, rec, thresh = precision_recall_curve(y_test,prob_preds[:,1])
-								pr_auc = auc(rec,prec)
-								results['roc_auc'].append(roc_auc)
-								results['pr_auc'].append(pr_auc)
-							else:
-								results['roc_auc'].append(-1)
-								results['pr_auc'].append(-1 )
-
-
-							
-
-						if max_vertex_scale*max_vertex_moment>10:
-							for model in tqdm.tqdm(model_names,leave = False,desc = 'pca models'):
+							for model in tqdm.tqdm(model_names,leave=False,desc = 'simple models'):
 								if model in ['SVC','RBF']:
 									grid = svc_grid
 								else:
 									grid = lr_grid
+								
+								cv_model = GridSearchCV(simple_tuples[model], grid)
+								cv_model.fit(X_train, y_train)
+
+
+
 							
+
+							
+
 								results['iter'].append(i)
 								results['model'].append(model)
 								results['max_vertex_scale'].append(max_vertex_scale)
 								results['max_vertex_moment'].append(max_vertex_moment)
 								results['centered_vertex_features'].append(center_vertex_features)
-								results['pca'].append("Yes")
+								results['pca'].append("No")
 
-								cv_model = GridSearchCV(pca_tuples[model], grid)
-								cv_model.fit(X_train,y_train)
-
+								
 								
 								preds = cv_model.best_estimator_.predict(X_test)
 
 								acc = accuracy_score(y_test, preds)
 								b_acc = balanced_accuracy_score(y_test,preds)
+
 								results['acc'].append(acc)
 								results['bal_acc'].append(b_acc)
-								
+
 								if model == 'LR':
 									prob_preds = cv_model.best_estimator_.predict_proba(X_test)
 									roc_auc = roc_auc_score(y_test, prob_preds[:,1])
@@ -187,57 +171,17 @@ def main():
 									results['pr_auc'].append(pr_auc)
 								else:
 									results['roc_auc'].append(-1)
-									results['pr_auc'].append(-1) 
+									results['pr_auc'].append(-1 )
+
 							
 
 
-	results = pd.DataFrame(results)
+		results = pd.DataFrame(results)
 
-	results.to_csv(path + "DW.csv", index = False)
+		results.to_csv(path + "{d}_DW.csv".format(d=dtype), index = False)
 		
 if __name__ == '__main__':
 	main()
 
 
-
-if task == 'classification':
-							preds = cv_model.best_estimator_.predict(X_test)
-							pred_probs = cv_model.best_estimator_.predict_proba(X_test)
-							acc = accuracy_score(y_test, preds)
-							roc = roc_auc_score(y_test, pred_probs[:,1])
-							tn, fp, fn, tp  = confusion_matrix(y_test, preds, labels = [0,1]).ravel()
-							f1 = f1_score(y_test,preds)
-
-							results['test_acc'].append(acc)
-							results['test_roc'].append(roc)
-							results['true negatives'].append(tn)
-							results['false_negatives'].append(fn)
-							results['true_positives'].append(tp)
-							results['false_positives'].append(fp)
-							results['f1'].append(f1)
-
-
-
-
-
-
-
-
-
-							if task == 'classification':
-								preds = pca_cv.best_estimator_.predict(X_test)
-								pred_probs = pca_cv.best_estimator_.predict_proba(X_test)
-								
-								acc = accuracy_score(y_test, preds)
-								roc = roc_auc_score(y_test, pred_probs[:,1])
-								tn, fp, fn, tp  = confusion_matrix(y_test, preds, labels = [0,1]).ravel()
-								f1 = f1_score(y_test,preds)
-
-								results['test_acc'].append(acc)
-								results['test_roc'].append(roc)
-								results['true negatives'].append(tn)
-								results['false_negatives'].append(fn)
-								results['true_positives'].append(tp)
-								results['false_positives'].append(fp)
-								results['f1'].append(f1)
 
